cb3e2e77fce16b2654f3084ea93e7ef0b3cfe4ae
chrisbdevelopper <kenshin91cb@gmail.com>
2021-04-13 22:46:45 +0200

    MAJ DESKTOP modification de l'effet <masonry> pour la page home et galerie OK

--- src/Entity/TextMenuBurger.php
+++ src/Entity/TextMenuBurger.php
@@ -4,10 +4,14 @@ namespace App\Entity;
 
 use App\Repository\TextMenuBurgerRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=TextMenuBurgerRepository::class)
+ * @Vich\Uploadable
  */
 class TextMenuBurger
 {
@@ -23,6 +27,29 @@ class TextMenuBurger
      */
     private $text;
 
+    /**
+     * @var File|null
+     * @Vich\UploadableField(mapping="bio_img", fileNameProperty="fileName")
+     * @Assert\File(
+     *     maxSize = "40M",
+     *     mimeTypes = {"image/bmp", "image/png", "image/svg+xml", "image/jpx", "image/jpg", "image/jpeg"},
+     *     mimeTypesMessage = "Seul les formats suivant sont acceptés : jpeg/png/svg/bmp"
+     * )
+     */
+    private $file;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $fileName;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var \DateTimeInterface|null
+     */
+    private $updatedAt;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -37,6 +64,38 @@ class TextMenuBurger
     {
         $this->text = $text;
 
+        return $this;
+    }
+
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+
+    public function setFile(?File $file = null): void
+    {
+        $this->file = $file;
+        if ($this->file instanceof UploadedFile) {
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFileName(): ?string
+    {
+        return $this->fileName;
+    }
+
+    /**
+     * @param string|null $fileName
+     * @return TextMenuBurger
+     */
+    public function setFileName(?string $fileName): TextMenuBurger
+    {
+        $this->fileName = $fileName;
         return $this;
     }
 }
--- src/Form/TextMenuBurgerType.php
+++ src/Form/TextMenuBurgerType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\TextMenuBurger;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -25,6 +26,18 @@ class TextMenuBurgerType extends AbstractType
                 'required' => true,
 //                'help' => 'Entrez les éléments du menu "slide"',
 //                'help_attr' => ['class' => 'help'],
+            ])
+            ->add('file', FileType::class,[
+                'error_bubbling' => true,
+                'label' => 'Fichier image',
+                'label_attr' => ['class'=>'has-text-weight-bold'],
+                'row_attr' => ['class'=>'field is-flex is-flex-direction-column'],
+                'attr' => [
+                    'class' => 'has-text-centered'
+                ],
+                'required' => false,
+                'help' => 'Types de fichier autorisé : .jpg .png .svg .bmp',
+                'help_attr' => ['class' => 'help']
             ])
         ;
     }
