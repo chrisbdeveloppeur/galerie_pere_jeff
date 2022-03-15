501e621e390ea1a0a7887157e0ba03e729b433c4
K4gur0 <kenshin91cb@gmail.com>
2022-03-15 14:12:34 +0100

    LAPTOP prepare for deploy to toupet sr

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avz ./ -e "ssh -p 22"  admin@82.66.72.97:/var/www/html/site_toupet/ --include=public/build --include=public/bundles --include=public/.htaccess --include=vendor --exclude-from=.gitignore --exclude="public/images"
+rsync -avz ./ -e "ssh -p 22"  admindev@82.66.72.97:/var/www/my_webapp/www/ --include=public/build --include=public/bundles --include=public/.htaccess --include=vendor --exclude-from=.gitignore --exclude="public/images"
