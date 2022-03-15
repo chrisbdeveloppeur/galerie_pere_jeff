7ee74ce2b83c5493385ef21a1d818821a40da4ed
chrisbdevelopper <kenshin91cb@gmail.com>
2022-03-15 22:14:31 +0100

    DESKTOP change shortcut putty

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avz ./ -e "ssh -p 22"  admindev@82.66.72.97:/var/www/my_webapp/www/ --include=public/build --include=public/bundles --include=public/.htaccess --include=vendor --include=vendor --exclude="public/images" --exclude="Putty - Web Toupet.Ink"
+rsync -avz ./ -e "ssh -p 22"  admindev@82.66.72.97:/var/www/my_webapp/www/ --include=public/build --include=public/bundles --include=public/.htaccess --include=vendor --include=vendor --exclude="public/images" --exclude=.gitignore --exclude=.* --exclude="Putty - Web Toupet.Ink"
