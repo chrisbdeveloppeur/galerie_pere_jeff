fe343ebe9ae752cd5e82795af6a7ce53ba0dfd9f
K4gur0 <kenshin91cb@gmail.com>
2022-03-16 13:29:14 +0100

    LAPTOP modif deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avz ./ -e "ssh -p 22"  admindev@82.66.72.97:/var/www/my_webapp/www/ --include=public/build --include=public/bundles --include="public/.htaccess" --include=vendor --exclude="public/images" --exclude=.gitignore --exclude="/.env.local" --exclude="/putty_web_toupet.Ink"
+rsync -avz ./ -e "ssh -p 22"  admindev@82.66.72.97:/var/www/my_webapp/www/ --include=public/build --include=public/bundles --include="public/.htaccess" --include=vendor --exclude="public/images" --exclude=.gitignore --exclude="/.env.local" --exclude="/putty_web_toupet.lnk"
