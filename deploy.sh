6f8d647ae7e61b51251a69125e4867e3c4384862
K4gur0 <kenshin91cb@gmail.com>
2023-06-12 19:51:50 -0400

    LAPTOP
    Deploy - maj pour mise en prod planethoster

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avz ./ -e "ssh -p 5022"  ahqxqjrt@node48-eu.n0c.com:~/public_html/jacques-toupet.com --include=public/build --include=public/bundles --include=public/.htaccess --include=.htaccess --include=.env --exclude=vendor --exclude-from=.gitignore --exclude=".*" --exclude=".env.local" --exclude=deploy.sh
+rsync -avz ./ -e "ssh -p 5022"  ahqxqjrt@node48-eu.n0c.com:~/public_html/jacques-toupet.com --include=public/build --include=public/bundles --include=public/.htaccess --include=.htaccess --include=.env --exclude=".env.local" --exclude=deploy.sh
